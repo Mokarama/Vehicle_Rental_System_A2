f923f73cabbf3e6818de9599d6f353e1ea3600f5
Mokarama <mokaramaaktershanta@gmail.com>
2026-04-17 06:15:48 +0600

    fix port for render

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import app from "./app";
-import config from "./config";
 import initDB from "./config/db";
 
-const port = config.port || 4000;
+const port = process.env.PORT || 4000;
 
 const startServer = async () => {
-  await initDB(); // DB init
+  await initDB();
 
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
